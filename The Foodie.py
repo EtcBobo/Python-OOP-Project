32aec9285cf862870be1134a25d450f53963e72a
Shawn (Chiabobo) <sh_ibo@live.com>
2017-12-10 18:24:39 +0800

    Update
    correct the filter function

--- The Foodie.py
+++ The Foodie.py
@@ -22,7 +22,7 @@ class RegisterForm(Form):
     user = StringField('Username',[validators.DataRequired()])
     password = PasswordField("Password",[validators.DataRequired()])
     price = StringField('Preferred Price Range')
-    foodType = SelectField(u'Food Types',
+    foodType = SelectField(u'Preferred Food Type',
                            choices=[('Halal', 'Halal'), ('Vegetarian', 'Vegetarian'), ('Western Food', 'Western Food'),
                                     ('Chinese Food', 'Chinese Food'), ('Healthy Food', 'Healthy Food'),
                                     ('None', 'None')])
@@ -44,13 +44,13 @@ class RestForm(Form):
                                     ('9 AM', '9 AM'), ('10 AM', '10 AM'), ('11 AM', '11 AM'), ('12 PM', '12 PM'),
                                     ('1 PM', '1 PM'),('2 PM', '2 PM'),('3 PM', '3 PM'),('4 PM', '4 PM'),('5 PM', '5 PM'),('6 PM', '6 PM'),('7 PM', '7 PM'),
                                     ('8 PM', '8 PM'),('9 PM', '9 PM'),('10 PM', '10 PM'),('11 PM', '11 PM')])
-    closingH = SelectField(u'Opening Hours',
+    closingH = SelectField(u'Closing Hours',
                            choices=[('12 AM', '12 AM'),('1 AM', '1 AM'), ('2 AM', '2 AM'), ('3 AM', '3 AM'), ('4 AM', '4 AM'),
                                     ('5 AM', '5 AM'), ('6 AM', '6 AM'), ('7 AM', '7 AM'), ('8 AM', '8 AM'),
                                     ('9 AM', '9 AM'), ('10 AM', '10 AM'), ('11 AM', '11 AM'), ('12 PM', '12 PM'),
                                     ('1 PM', '1 PM'),('2 PM', '2 PM'),('3 PM', '3 PM'),('4 PM', '4 PM'),('5 PM', '5 PM'),('6 PM', '6 PM'),('7 PM', '7 PM'),
                                     ('8 PM', '8 PM'),('9 PM', '9 PM'),('10 PM', '10 PM'),('11 PM', '11 PM'),])
-    openT = SelectField(u'Opening Hours',
+    openT = SelectField(u'Operating Time',
                            choices=[('12 AM', '12 AM'),('1 AM', '1 AM'), ('2 AM', '2 AM'), ('3 AM', '3 AM'), ('4 AM', '4 AM'),
                                     ('5 AM', '5 AM'), ('6 AM', '6 AM'), ('7 AM', '7 AM'), ('8 AM', '8 AM'),
                                     ('9 AM', '9 AM'), ('10 AM', '10 AM'), ('11 AM', '11 AM'), ('12 PM', '12 PM'),
@@ -79,9 +79,6 @@ def filter():
 
         restFire = firebase.FirebaseApplication("https://python-oop.firebaseio.com/")
         totalRest = restFire.get('restaurants',None)
-        for key in totalRest:
-
-            print(totalRest[key]['Opening Hours'])
 
         for key in totalRest:
             if totalRest[key]['Opening Hours'][-2:] == 'PM':
@@ -102,18 +99,34 @@ def filter():
             if openH <= openT1 < closingH or openT1 < closingH < openH or openT1 > openH > closingH or closingH == openH:
                 filterList.append(totalRest[key])
 
-        for key in filterList:
-            if location != key['Location']:
-                filterList.remove(key)
+        i = 0
+        while i < len(filterList):
+            if filterList[i]['Location'] != location:
+                del filterList[i]
+                i = i - 1
+            i = i + 1
+
+        print(filterList)
+
+
         if price != '':
-            for key in filterList:
-                if price > key['Price']:
-                    filterList.remove(key)
+            i = 0
+            while i < len(filterList):
+                if int(filterList[i]['Price']) > int(price):
+                    del filterList[i]
+                    i = i - 1
+                i = i + 1
+
+            print(filterList)
 
         if foodType != 'None':
-            for key in filterList:
-                if foodType != key['Food Type']:
-                    filterList.remove(key)
+            i = 0
+            while i < len(filterList):
+                if filterList[i]['Food Types'] != foodType:
+                    del filterList[i]
+                    i = i - 1
+                i = i + 1
+
 
         session['filtered'] = filterList
         print(session['filtered'])
--- firebase test.py
+++ firebase test.py
@@ -17,8 +17,31 @@ default_app = firebase_admin.initialize_app(cred, {
 # totalRes = restFire.get('restaurants', None)
 # for key in totalRes:
 #     print(totalRes[key])
+location ='North'
+filterList = [{'Address': '', 'Closing Hours': '12 AM', 'Comments': '', 'Description': 'donald', 'Food Type': 'Western Food', 'Location': 'North', 'Name': 'Mac', 'Opening Hours': '12 AM', 'Price': '10'},
+              {'Address': '', 'Closing Hours': '10 PM', 'Comments': '', 'Description': 'chicken', 'Food Type': 'Chinese Food', 'Location': 'South', 'Name': 'KFC', 'Opening Hours': '8 AM', 'Price': '15'},
+              {'Address': '', 'Closing Hours': '10 PM', 'Comments': '', 'Description': 'dian xin', 'Food Type': 'Chinese Food', 'Location': 'West', 'Name': 'Dim Sum', 'Opening Hours': '7 AM', 'Price': '50'},
+              {'Address': '', 'Closing Hours': '12 PM', 'Comments': '', 'Description': 'yaki', 'Food Type': 'Healthy Food', 'Location': 'North', 'Name': 'Tako', 'Opening Hours': '1 AM', 'Price': '2'},
+              {'Address': '', 'Closing Hours': '12 AM', 'Comments': '', 'Description': 'queen', 'Food Type': 'Western Food', 'Location': 'North', 'Name': 'Burger King', 'Opening Hours': '12 AM', 'Price': '20'},
+              {'Address': 'Ang Moh Kio Mrt station', 'Closing Hours': '10 PM', 'Comments': '', 'Description': 'Japan imported', 'Food Type': 'None', 'Location': 'Central', 'Name': 'Dorayaki', 'Opening Hours': '8 AM', 'Price': '3'}]
+#
+# for i in range(6):
+#     if location != filterList[i]['Location']:
+#         del filterList[i]
+#     print(i)
 
-test = '10 am'
-test2 = '1 am'
-print(test2[-1:])
+
+i = 0
+while i < len(filterList):
+    if filterList[i]['Location'] != location:
+        del filterList[i]
+        i = i -1
+    i = i+1
+
+print(filterList)
+
+
+
+
+
 
