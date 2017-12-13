3871e1f990554d27a18c14ccff23a317b9f7311f
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2017-12-13 22:14:46 +0800

    minor update

--- The Foodie.py
+++ The Foodie.py
@@ -281,60 +281,46 @@ def userRegister():
         foodType = form.foodType.data
         email = form.email.data
 
-
-        email_user = 'thefoodie.newsletter@gmail.com'
-        email_password = 'foodie123'
-        email_send = email
-
-        subject = 'Warm welcome from TheFoodie team!'
-
-        msg = MIMEMultipart()
-        msg['From'] = email_user
-        msg['To'] = email_send
-        msg['Subject'] = subject
-
-        body = '\n Hi '  + user + '! Thank you for subscribing with TheFoodie Newsletter! \n As a welcome gift, get a free sundae and mcwings by using the codes given below ' \
-               '\n \nPlease continue to look forward to our monthly newsletters and get exclusive promotion codes only for subscribers! ' \
-               '\n \n \n Sincerely, ' \
-               '\n \nThenFoodie Team'
-
-        msg.attach(MIMEText(body, 'plain'))
-
-        filename = 'promo.jpg'
-        attachment = open(filename, 'rb')
-
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= " + filename)
-
-        msg.attach(part)
-        text = msg.as_string()
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(email_user, email_password)
-
-        server.sendmail(email_user, email_send, text)
-        server.quit()
-=======
         passwordC = form.passwordC.data
         if password != passwordC:
             flash('The passwords does not match')
             return redirect(url_for('userRegister'))
         if email != '':
+
+            email_user = 'thefoodie.newsletter@gmail.com'
+            email_password = 'foodie123'
+            email_send = email
+
+            subject = 'Warm welcome from TheFoodie team!'
+
+            msg = MIMEMultipart()
+            msg['From'] = email_user
+            msg['To'] = email_send
+            msg['Subject'] = subject
+
+            body = '\n Hi '  + user + '! Thank you for subscribing with TheFoodie Newsletter! \n As a welcome gift, get a free sundae and mcwings by using the codes given below ' \
+                   '\n \nPlease continue to look forward to our monthly newsletters and get exclusive promotion codes only for subscribers! ' \
+                   '\n \n \n Sincerely, ' \
+                   '\n \nThenFoodie Team'
+
+            msg.attach(MIMEText(body, 'plain'))
+
+            filename = 'promo.jpg'
+            attachment = open(filename, 'rb')
+
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload((attachment).read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', "attachment; filename= " + filename)
+
+            msg.attach(part)
+            text = msg.as_string()
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
-            server.login("omgnooopython@gmail.com", "pythonnopython")
-            msg = EmailMessage()
-            msg['Subject'] = 'The Foodie'
-            msg['From'] = 'thefoodie.newsletter@gmail.com'
-            msg['To'] = email
+            server.login(email_user, email_password)
 
-            msg.set_content(
-                "Thank you for subscribing with The Foodie Newsletter! Look forward to monthly newsletters and also exclusive discount codes for subscribers!")
-            server.send_message(msg)
+            server.sendmail(email_user, email_send, text)
             server.quit()
->>>>>>> 7fed95b9c29833fbbba7e2ba0591625a1d20848e
 
         reg = Registration(user,password,price,foodType,email)
 
