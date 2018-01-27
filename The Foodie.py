5eb24ddbae624706759c0dec67b31ac7f4e0c7c4
yyiiinn <33303522+yyiiinn@users.noreply.github.com>
2018-01-28 04:31:25 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	templates/userProfile.html

--- The Foodie.py
+++ The Foodie.py
@@ -113,7 +113,7 @@ class EventForm(Form):
                                     ('9 AM', '9 AM'), ('10 AM', '10 AM'), ('11 AM', '11 AM'), ('12 PM', '12 PM'),
                                     ('1 PM', '1 PM'),('2 PM', '2 PM'),('3 PM', '3 PM'),('4 PM', '4 PM'),('5 PM', '5 PM'),('6 PM', '6 PM'),('7 PM', '7 PM'),
                                     ('8 PM', '8 PM'),('9 PM', '9 PM'),('10 PM', '10 PM'),('11 PM', '11 PM')])
-    endTime =  SelectField(u'Start Time(Hr)*',
+    endTime =  SelectField(u'End Time(Hr)*',
                            choices=[('12 AM', '12 AM'),('1 AM', '1 AM'), ('2 AM', '2 AM'), ('3 AM', '3 AM'), ('4 AM', '4 AM'),
                                     ('5 AM', '5 AM'), ('6 AM', '6 AM'), ('7 AM', '7 AM'), ('8 AM', '8 AM'),
                                     ('9 AM', '9 AM'), ('10 AM', '10 AM'), ('11 AM', '11 AM'), ('12 PM', '12 PM'),
