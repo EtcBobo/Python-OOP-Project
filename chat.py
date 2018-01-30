a38efd2f4f9a98ac4162ca4b15fef72c549f1da1
pyaepyaelily <pyaepyaelily@gmail.com>
2018-01-30 13:24:53 +0800

    Added explanation for chat

--- chat.py
+++ chat.py
@@ -11,8 +11,7 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'jsbcfsbfjefebw237u3gdbdc'
-socketio = SocketIO( app )
-
+socketio = SocketIO( app ) #wrap the app using the socketio
 
 @app.route('/chat')
 def hello():
@@ -22,10 +21,12 @@ def hello():
 def messagereceived():
   print( 'message was received!!!' )
 
-@socketio.on( 'my event' )
-def handle_my_custom_event( json ):
-  print( 'recived my event: ' + str( json ) )
-  socketio.emit( 'my response', json, callback=messagereceived())
+@socketio.on( 'my event' ) #To receive WebSocket messages from the client the application defines event handlers using the socketio.on decorator ,here we need to receive this event. and send it back to the server
 
+def handle_my_custom_event( json ):
+  print( 'recived my event: ' + str( json ) ) #call json response  and cast the json to string
+  socketio.emit( 'my response', json, callback=messagereceived()) #sending a new msg from the server to this event.
+
+#The emit() function sends a message under a custom event name.
 if __name__ == '__main__':
-  socketio.run( app, debug = True )
+  socketio.run( app, debug = True ) #
